7d1e0361415bdbf3a88ed865a2217ef47dbd205e
saraan <saraansatyal@gmail.com>
2018-02-25 04:42:26 +0545

    manage.py updated

--- codesmartproject/settings.py
+++ codesmartproject/settings.py
@@ -117,8 +117,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
